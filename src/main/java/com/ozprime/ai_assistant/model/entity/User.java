7ed94c1d558d00f6ec8c6e09fef8ebef5a2ecb9e
Oshadha <moshadha23@gmail.com>
2026-03-07 19:09:12 +0530

    add User entity with JPA annotations and lifecycle methods

---
+++ src/main/java/com/ozprime/ai_assistant/model/entity/User.java
@@ -0,0 +1,39 @@
+package com.ozprime.ai_assistant.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+@Entity
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+    }
+}
